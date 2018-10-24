82d4e9e78a9153b843cdffcd47f1005a34948b75
Ashley <ash.1303.github@dsouzaville.com>
2018-10-24 16:00:11 -0500

    tweak whitespace

--- build/resume.js
+++ build/resume.js
@@ -9148,23 +9148,27 @@ var _user$project$Resume$theResume = {
 	header: _user$project$Resume$header,
 	body: {
 		ctor: '::',
-		_0: A2(_user$project$ResumeTypes$NestedSection, 'Education', _user$project$Resume$education),
+		_0: A2(_user$project$ResumeTypes$FlatSection, 'Why Spotify?', _user$project$Resume$whySpotify),
 		_1: {
 			ctor: '::',
-			_0: A2(_user$project$ResumeTypes$NestedSection, 'Conference Presentations', _user$project$Resume$awards),
+			_0: A2(_user$project$ResumeTypes$NestedSection, 'Education', _user$project$Resume$education),
 			_1: {
 				ctor: '::',
-				_0: A2(_user$project$ResumeTypes$NestedSection, 'Projects', _user$project$Resume$projects),
+				_0: A2(_user$project$ResumeTypes$NestedSection, 'Conference Presentations', _user$project$Resume$awards),
 				_1: {
 					ctor: '::',
-					_0: A2(_user$project$ResumeTypes$NestedSection, 'Software Development Skills', _user$project$Resume$programming),
+					_0: A2(_user$project$ResumeTypes$NestedSection, 'Projects', _user$project$Resume$projects),
 					_1: {
 						ctor: '::',
-						_0: A2(_user$project$ResumeTypes$NestedSection, 'Work Experience', _user$project$Resume$work),
+						_0: A2(_user$project$ResumeTypes$NestedSection, 'Software Development Skills', _user$project$Resume$programming),
 						_1: {
 							ctor: '::',
-							_0: A2(_user$project$ResumeTypes$FlatSection, 'Organizations and Activities', _user$project$Resume$activities),
-							_1: {ctor: '[]'}
+							_0: A2(_user$project$ResumeTypes$NestedSection, 'Work Experience', _user$project$Resume$work),
+							_1: {
+								ctor: '::',
+								_0: A2(_user$project$ResumeTypes$FlatSection, 'Organizations and Activities', _user$project$Resume$activities),
+								_1: {ctor: '[]'}
+							}
 						}
 					}
 				}
